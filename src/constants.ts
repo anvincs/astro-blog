d64c3cbacc771a9274ee362e640c7cf48dd97ffd
Anvin.C.Shaju <anvincsg@gmail.com>
2023-12-19 14:27:50 +0530

    Create Article Card component

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = "Tech People Blog";
 export const SITE_DESCRIPTION =
   "Articles, Stories and Tutorials from Tech People";
-export const HOMEPAGE_ARTICLES_LIMIT = 7;
+export const HOMEPAGE_ARTICLES_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
